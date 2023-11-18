ac559430bc8dcb070d76c4f35e7ad5d3b1d9a950
dannyMAC <d4nny815@gmail.com>
2023-11-18 03:22:10 -0800

    esp_now control box

--- data_collection/format.py
+++ data_collection/format.py
@@ -1,16 +1,24 @@
 import sys
 
-data_format = "time(msec),fuel_pressure(psi),chamber_pressure(psi),oxygen_pressure(psi),loadcell(lbs)\n"
+data_format = "time(msec),fuel_pressure(psi),chamber_pressure(psi),\
+                oxygen_pressure(psi),loadcell(lbs)\n"
 
 def main():
+    if (len(sys.argv) != 2):
+        print("Usage: python format.py <input_file>")
+        return
     input_file = sys.argv[1].rstrip(".txt").split('_')
-    type_of_run = input_file[0]
-    run_number = input_file[1]
-    date = input_file[2]
-    output_file_name = "formatted_" + type_of_run + "_" + run_number + "_" + date + ".csv"
+    date = input_file[0]
+    type_of_run = input_file[1]
+    run_number = input_file[2]
+    output_file_name = date + "_" + type_of_run + "_" + run_number + "_formatted" + ".csv"
     
     out_file = open(output_file_name, "w")
-    in_file = open(sys.argv[1], "r")
+    try:
+        in_file = open(sys.argv[1], "r")
+    except:
+        print("Error: File not found OR invalid file format: <date>_<type_of_run>_<run_number>.txt")
+        return
     start_time = int(in_file.readline().split(",")[0])
     out_file.write(data_format)
 
@@ -23,12 +31,9 @@ def main():
             break
         line[0] = time - start_time
         out_file.write(','.join([str(i) for i in line]))
-        
-        
     in_file.close()
     out_file.close()
     return
-
 
 if __name__ == "__main__":
     main()
--- esp_comms/src/main.cpp
+++ esp_comms/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <DigitalInput.h>
+#include <WiFi.h>
+#include <esp_now.h>
 
 // INPUT PINS
 //Digital IN
@@ -9,8 +11,6 @@
 #define SW_FUEL 19
 #define BTN_LAUNCH 18
 #define SW_KSI 5
-
-#define BAUD_RATE 9600 //
 
 // Instantiate every component in the system
 DigitalInput  ign_sw(SW_IGN);
@@ -35,81 +35,123 @@ const u_int8_t bit_valid = 1 << 6;
 u_int8_t bit_heartbeat = 1 << 7;
 
 
+uint32_t time_data = 0;
+uint16_t ducer1_data = 0;
+uint16_t ducer2_data = 0;
+uint16_t ducer3_data = 0;
+uint16_t ducer4_data = 0;
+uint16_t ducer5_data = 0;
+uint16_t ducer6_data = 0;
+uint16_t thermoresistor1_data = 0;
+uint16_t thermoresistor2_data = 0;
+uint16_t thermoresistor3_data = 0;
+uint16_t loadcell_data = 0;
+String received_data;
+
+
+esp_now_peer_info_t peerInfo;
+uint8_t broadcastAddress[] = {0x08, 0xd1, 0xf9, 0xef, 0x32, 0x84};
+
+typedef struct {
+	uint8_t button_data;
+	uint32_t time_data;
+	uint16_t transducer1_data;
+	uint16_t transducer2_data;
+	uint16_t transducer3_data;
+	uint16_t transducer4_data;
+	uint16_t transducer5_data;
+	uint16_t transducer6_data;
+	uint16_t thermoresistor1_data;
+	uint16_t thermoresistor2_data;
+	uint16_t thermoresistor3_data;
+	uint16_t loadcell_data;
+} dataPacket;
+
+dataPacket incomingPacket[8];
+dataPacket outgoingPacket;
+String success;
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+//   Serial.print("\r\nLast Packet Send Status:\t");
+//   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+esp_err_t result;
+
+
+int buffer_index;
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  	memcpy(&incomingPacket, incomingData, sizeof(incomingPacket));
+  	// Serial.print("Bytes received: ");
+  	// Serial.println(len);
+	
+	for (buffer_index = 0; buffer_index < 8; buffer_index++) { 
+		Serial.printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n", incomingPacket[buffer_index].time_data, incomingPacket[buffer_index].transducer1_data,
+						incomingPacket[buffer_index].transducer2_data, incomingPacket[buffer_index].transducer3_data, incomingPacket[buffer_index].transducer4_data, 
+						incomingPacket[buffer_index].transducer5_data, incomingPacket[buffer_index].transducer6_data, incomingPacket[buffer_index].thermoresistor1_data, 
+						incomingPacket[buffer_index].thermoresistor2_data, incomingPacket[buffer_index].thermoresistor3_data, incomingPacket[buffer_index].loadcell_data);
+	}
+	return;
+}
+
 unsigned long cur_time;
 unsigned long prev_time_sent = millis();
 unsigned long transmit_time = 50;
 
-byte buffer[16];
-String timeString;
-uint64_t time_data = 0;
-uint16_t ducer1_data = 0;
-uint16_t ducer2_data = 0;
-uint16_t ducer3_data = 0;
-uint16_t loadcell_data = 0;
-String received_data;
-
 void setup() {
-  	Serial.begin(115200);
-  	Serial2.begin(BAUD_RATE);
+  	Serial.begin(230400);
 	Serial.println("POWER_ON");
-	transmit_data |= bit_valid;
-  	transmit_data |= bit_heartbeat;
+
+	WiFi.mode(WIFI_STA);
+	esp_now_init();
+    esp_now_register_send_cb(OnDataSent);
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+	esp_now_add_peer(&peerInfo);
+    esp_now_register_recv_cb(OnDataRecv);
+
+	outgoingPacket.button_data |= bit_valid;
+  	outgoingPacket.button_data |= bit_heartbeat;
 }
 
 void loop() {
   	// read the KSI bit
-	if (ksi_sw.read()) transmit_data = transmit_data|bit_ksi;
-	else transmit_data = transmit_data & (bit_ksi ^ 0xff);
+	// if (ksi_sw.read()) outgoingPacket.button_data = outgoingPacket.button_data|bit_ksi;
+	// else outgoingPacket.button_data = outgoingPacket.button_data & (bit_ksi ^ 0xff);
 
-	// read the IGNITION bit
-	if (launch_btn.read()) transmit_data = transmit_data|bit_launch_btn;
-	else transmit_data = transmit_data & (bit_launch_btn ^ 0xff);
+	// // read the IGNITION bit
+	// if (launch_btn.read()) outgoingPacket.button_data = outgoingPacket.button_data|bit_launch_btn;
+	// else outgoingPacket.button_data = outgoingPacket.button_data & (bit_launch_btn ^ 0xff);
 
-	// read the FUEL bit
-	if (fuel_sw.read()) transmit_data = transmit_data|bit_sw_fuel;
-	else transmit_data = transmit_data & (bit_sw_fuel ^ 0xff);
+	// // read the FUEL bit
+	// if (fuel_sw.read()) outgoingPacket.button_data = outgoingPacket.button_data|bit_sw_fuel;
+	// else outgoingPacket.button_data = outgoingPacket.button_data & (bit_sw_fuel ^ 0xff);
 
-	// read the OXYGEN bit
-	if (oxygen_sw.read()) transmit_data = transmit_data|bit_sw_oxygen;
-	else transmit_data = transmit_data & (bit_sw_oxygen ^ 0xff);
+	// // read the OXYGEN bit
+	// if (oxygen_sw.read()) outgoingPacket.button_data = outgoingPacket.button_data|bit_sw_oxygen;
+	// else outgoingPacket.button_data = outgoingPacket.button_data & (bit_sw_oxygen ^ 0xff);
 
-	//read the LAUNCH bit
-	if (launch_sw.read()) transmit_data = transmit_data|bit_sw_launch;
-	else transmit_data = transmit_data & (bit_sw_launch ^ 0xff);
+	// //read the LAUNCH bit
+	// if (launch_sw.read()) outgoingPacket.button_data = outgoingPacket.button_data|bit_sw_launch;
+	// else outgoingPacket.button_data = outgoingPacket.button_data & (bit_sw_launch ^ 0xff);
 
-	//read the IGNITION bit
-	if (ign_sw.read()) transmit_data = transmit_data|bit_sw_ign;
-	else transmit_data = transmit_data & (bit_sw_ign ^ 0xff);
+	// //read the IGNITION bit
+	// if (ign_sw.read()) outgoingPacket.button_data = outgoingPacket.button_data|bit_sw_ign;
+	// else outgoingPacket.button_data = outgoingPacket.button_data & (bit_sw_ign ^ 0xff);
 
-  	cur_time = millis();
-  	if (cur_time - prev_time_sent >= transmit_time) {
-  	  	prev_time_sent = cur_time;
-		if (transmit_data != prev_transmit_data) {
-			prev_transmit_data = transmit_data;
-			// Serial.println(transmit_data, BIN);
-			Serial2.write(transmit_data);
-		}
-
-  	}
-
-	if (Serial2.available()){
-  	    Serial2.readBytes(buffer, 16);
-
-  	    time_data = buffer[0] << 56 | buffer[1] << 48 | buffer[2] << 40 
-							| buffer[3] << 32 | buffer[4] << 24 | buffer[5] << 16 
-							| buffer[6] << 8 | buffer[7];
-		ducer1_data = buffer[8] << 8 | buffer[9];
-		ducer2_data = buffer[10] << 8 | buffer[11];
-		ducer3_data = buffer[12] << 8 | buffer[13];
-		loadcell_data = buffer[14] << 8 | buffer[15];
-
-
-  	  	received_data = String(time_data) + "," + String(ducer1_data) + "," 
-  	                + String(ducer2_data) + "," + String(ducer3_data) + ","
-  	                 + String(loadcell_data);
-  	  	Serial.println(received_data);
-
-  	  	time_data = ducer1_data = ducer2_data = ducer3_data = loadcell_data = 0;
-  	}
-
+  	if (Serial.available()) {
+		uint8_t incomingByte = Serial.read();
+		outgoingPacket.button_data = incomingByte;
+	}
+	cur_time = millis();
+	if (cur_time - prev_time_sent >= transmit_time) {
+		prev_time_sent = cur_time;
+		result = esp_now_send(broadcastAddress, (uint8_t *) &outgoingPacket, sizeof(outgoingPacket));
+		// if (result == ESP_OK) {
+    	//     Serial.println("Sent with success");
+    	// }
+		// else {
+    	//     Serial.println("Error sending the data");
+		// }
+	}
 }
