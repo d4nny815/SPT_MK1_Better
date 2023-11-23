39be2b849e67ec75ccc20d593f077071d8cb6360
dannyMAC <d4nny815@gmail.com>
2023-11-22 16:29:26 -0800

    control Box

--- Slave_control_box/lib/DigitalInput/DigitalInput.cpp
+++ Slave_control_box/lib/DigitalInput/DigitalInput.cpp
@@ -13,25 +13,19 @@ DigitalInput::DigitalInput(int pin) {
 }
 
 /**
- * @brief Reads the state of the digital input and debounces it
+ * @brief Reads the digital input
  * @return The state of the digital input
+ * @note This function also debounces the input
 */
 bool DigitalInput::read() {
     _btn_state = digitalRead(_pin);
     _cur_time = micros();
 
-    // Check if the button state has changed
-    if (_btn_state != _last_btn_state) {
-        // Record the time of the state change for debounce
-        _prev_time_check = micros();
-    }
-
-    // Check if enough time has passed since the last state change
     if (_cur_time - _prev_time_check >= _debounce_time_us) {
-        // Return the current button state
+        _prev_time_check = _cur_time;
+        _last_btn_state = _btn_state;
         return _btn_state;
     }
 
-    // Return the previous button state if debounce time hasn't passed
     return _last_btn_state;
 }
---
+++ Slave_control_box/lib/SolenoidValve/SolenoidValve.cpp
@@ -0,0 +1,54 @@
+#include "SolenoidValve.h"
+#include <Arduino.h>
+
+
+/**
+ * @brief Constructor for SoleinoidValve class
+ * @param pin The pin number of the digital input
+ * @param pullin_voltage The voltage percentage to turn on the solenoid valve
+ * @param holding_voltage The voltage percentage to hold the solenoid valve
+ * @return SoleinoidValve object
+*/
+SoleinoidValve::SoleinoidValve(int pin, uint8_t pullin_voltage, uint8_t holding_voltage) {
+    _pin = pin;
+    _IO_state = 0;
+    _pull_in_voltage = (uint8_t)(constrain(pullin_voltage, 0, 100) / 100.0 * 255);
+    _holding_voltage = (uint8_t)(constrain(holding_voltage, 0, 100) / 100.0 * 255);
+    _in_pull_in_state = false;
+    pinMode(_pin, OUTPUT);
+    analogWrite(_pin, 0);
+}
+
+bool SoleinoidValve::get_state() {return _IO_state; }
+
+
+/**
+ * @brief turn on the solenoid valve
+*/
+void SoleinoidValve::open() {
+    if (_IO_state) return;
+    if (!_in_pull_in_state) {
+        analogWrite(_pin, _pull_in_voltage);
+        _in_pull_in_state = true;
+        _previous_time_check = millis();
+        return;
+    }
+    _current_time = millis();
+    if (_current_time - _previous_time_check < _pull_in_time_interval) return;
+    _previous_time_check = _current_time;
+    analogWrite(_pin, _holding_voltage);
+    _IO_state = 1;
+    return;
+}
+
+
+/**
+ * @brief turn off the solenoid valve
+*/
+void SoleinoidValve::close() {
+    if (!_IO_state) return;
+    _IO_state = 0;
+    _in_pull_in_state = false;
+    analogWrite(_pin, 0);
+    return;
+}
